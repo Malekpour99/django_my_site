d24c6f62a152e28d240f7351147816b10c35fa97
Malekpour99 <103121845+Malekpour99@users.noreply.github.com>
2023-04-29 05:46:26 +0330

    Created some simple HTTP responses to see if our routes and views work

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Home page")
+
+def posts(request):
+    return HttpResponse("Posts page")
+
+def post(request, slug):
+    return HttpResponse("individual post")
