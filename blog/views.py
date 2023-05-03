e3a27e89181eed671c46acb9a1a757aebe1adb19
Malekpour99 <103121845+Malekpour99@users.noreply.github.com>
2023-05-03 15:03:18 +0330

    Added some dummy data and injected dynamic data to the starting template

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,78 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from datetime import date
+
+all_posts = [
+    {
+        "slug": "hike-in-the-mountains",
+        "image": "mountains.jpg",
+        "author": "Achilles",
+        "date": date(2021, 7, 21),
+        "title": "Mountain Hiking",
+        "excerpt": """There's nothing like the views you get when hiking in the mountains! 
+                    and I wasn't even prepared for what happened whilst I was enjoying the view!""",
+        "content": """Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit nostrum, iste fugiat quasi ipsa quod
+                    hic aut explicabo? Quis possimus tempora distinctio at dignissimos quas autem error impedit, eos tenetur.
+                    
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit nostrum, iste fugiat quasi ipsa quod
+                    hic aut explicabo? Quis possimus tempora distinctio at dignissimos quas autem error impedit, eos tenetur.
+                    
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit nostrum, iste fugiat quasi ipsa quod
+                    hic aut explicabo? Quis possimus tempora distinctio at dignissimos quas autem error impedit, eos tenetur."""
+    },
+    {
+        "slug": "programming-is-fun",
+        "image": "coding.jpg",
+        "author": "Achilles",
+        "date": date(2022, 3, 10),
+        "title": "Programming Is Great!",
+        "excerpt": """Did you ever spend hours searching that one error in your code? Yep - that's what happend to me yesterday...
+                    """,
+        "content": """Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit nostrum, iste fugiat quasi ipsa quod
+                    hic aut explicabo? Quis possimus tempora distinctio at dignissimos quas autem error impedit, eos tenetur.
+                    
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit nostrum, iste fugiat quasi ipsa quod
+                    hic aut explicabo? Quis possimus tempora distinctio at dignissimos quas autem error impedit, eos tenetur.
+                    
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit nostrum, iste fugiat quasi ipsa quod
+                    hic aut explicabo? Quis possimus tempora distinctio at dignissimos quas autem error impedit, eos tenetur."""
+    },
+    {
+        "slug": "into-the-woods",
+        "image": "woods.jpg",
+        "author": "Achilles",
+        "date": date(2020, 8, 5),
+        "title": "Nature At Its Best",
+        "excerpt": """Nature is amazing! the amount of inspiration I get when walking in nature is incredible!
+                    """,
+        "content": """Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit nostrum, iste fugiat quasi ipsa quod
+                    hic aut explicabo? Quis possimus tempora distinctio at dignissimos quas autem error impedit, eos tenetur.
+                    
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit nostrum, iste fugiat quasi ipsa quod
+                    hic aut explicabo? Quis possimus tempora distinctio at dignissimos quas autem error impedit, eos tenetur.
+                    
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit nostrum, iste fugiat quasi ipsa quod
+                    hic aut explicabo? Quis possimus tempora distinctio at dignissimos quas autem error impedit, eos tenetur."""
+    },
+]
+
+
+def get_date(post):
+    return post["date"]
 
 # Create your views here.
+
+
 def starting_page(request):
-    return render(request, "blog/index.html")
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/index.html", {
+        "posts": latest_posts
+    })
+
 
 def posts(request):
     return render(request, "blog/all-posts.html")
+
 
 def post_detail(request, slug):
     return render(request, "blog/post-detail.html")
