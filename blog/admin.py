8a7caf341369819cdc57a94d9f24a798893c2dba
Malekpour99 <103121845+Malekpour99@users.noreply.github.com>
2023-05-25 10:26:03 +0330

    Registered Post, Author & Tag models in the django admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Post, Author, Tag
 
 # Register your models here.
+
+
+# class PostAdmin(admin.ModelAdmin):
+#     readonly_fields = ()
+#     prepopulated_fields = {"slug": ("title", )}
+#     list_filter = ("date", )
+#     list_display = ("title", "excerpt", "date", )
+
+
+admin.site.register(Post)
+admin.site.register(Author)
+admin.site.register(Tag)
