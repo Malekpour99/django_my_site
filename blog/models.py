12b91ca2ca8e7011e6ff3409df8cfc5c33153a28
Malekpour99 <103121845+Malekpour99@users.noreply.github.com>
2023-06-13 04:26:28 +0330

    Changed the representation of the post model in the admin panel

--- blog/models.py
+++ blog/models.py
@@ -34,6 +34,9 @@ class Post(models.Model):
         Author, null=True, on_delete=models.SET_NULL, related_name="posts")
     tags = models.ManyToManyField(Tag)
 
+    def __str__(self) -> str:
+        return self.title
+
 
 class Comment(models.Model):
     user_name = models.CharField(max_length=120)
